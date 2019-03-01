4268f28311d80cd0b91e3d79585ca41c6e897a5f
davidgouge <davegouge@googlemail.com>
2019-03-01 15:19:56 +0000

    Created partial and tag helper to display GDS validation summary

---
+++ src/SFA.DAS.LoginService.Web/Views/TagHelpers/ValidationErrorListTagHelper.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SFA.DAS.LoginService.Web.Views.TagHelpers
+{
+    [HtmlTargetElement("sfa-validation-error-list")] 
+    public class ValidationErrorListTagHelper : TagHelper
+    {
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+        
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = null;
+            foreach (var modelState in ViewContext.ModelState)
+            {
+                foreach (var error in modelState.Value.Errors)
+                {
+                    var liTagBuilder = new TagBuilder("li");
+                    var aTagBuilder = new TagBuilder("a");
+                    aTagBuilder.Attributes.Add("href","#"+modelState.Key);
+                    aTagBuilder.InnerHtml.Append(error.ErrorMessage);
+
+                    liTagBuilder.InnerHtml.AppendHtml(aTagBuilder);
+                    
+                    output.Content.AppendHtml(liTagBuilder);
+                }
+            }
+        }
+    }
+}

f45cf9a9e11afd66ec195bf3996afe43907c786c
Greg Barnes <gregdbarnes@gmail.com>
2019-03-01 14:50:37 +0000

    Error message for confirm code, appsettings.json

--- src/SFA.DAS.LoginService.Web/Controllers/InvitationsWeb/ConfirmCodeController.cs
+++ src/SFA.DAS.LoginService.Web/Controllers/InvitationsWeb/ConfirmCodeController.cs
@@ -24,14 +24,14 @@ namespace SFA.DAS.LoginService.Web.Controllers.InvitationsWeb
         public async Task<ActionResult> Get(Guid invitationId)
         {
             var invitationResponse = await _mediator.Send(new GetInvitationByIdRequest(invitationId));
-            
+
             if (invitationResponse != null && !invitationResponse.IsUserCreated && invitationResponse.ValidUntil >= SystemTime.UtcNow())
             {
                 var confirmCodeViewModel = new ConfirmCodeViewModel(invitationResponse.Id, "");
                 return View("ConfirmCode", confirmCodeViewModel);
             }
-            
-            return View("InvitationExpired", new InvitationExpiredViewModel(){InvitationId = invitationId});
+
+            return View("InvitationExpired", new InvitationExpiredViewModel() { InvitationId = invitationId });
         }
 
         [HttpPost("/Invitations/ConfirmCode/{invitationId}")]
@@ -39,39 +39,39 @@ namespace SFA.DAS.LoginService.Web.Controllers.InvitationsWeb
         {
             if (string.IsNullOrWhiteSpace(confirmCodeViewModel.Code))
             {
-                ModelState.AddModelError("Code","Please supply code");
-            
+                ModelState.AddModelError("Code", "Enter confirmation code");
+
                 return View("ConfirmCode", confirmCodeViewModel);
             }
-            
+
             var confirmCodeResponse = await _mediator.Send(new ConfirmCodeRequest(confirmCodeViewModel.InvitationId, confirmCodeViewModel.Code));
             if (confirmCodeResponse.IsValid)
             {
-                return RedirectToAction("Get", "CreatePassword", new {id = confirmCodeViewModel.InvitationId});
+                return RedirectToAction("Get", "CreatePassword", new { id = confirmCodeViewModel.InvitationId });
             }
 
-            ModelState.AddModelError("Code","Code not valid");
-            
+            ModelState.AddModelError("Code", "Code not valid");
+
             return View("ConfirmCode", confirmCodeViewModel);
         }
 
         [HttpPost("/Invitations/Reinvite/{invitationId}")]
         public async Task<IActionResult> Reinvite(Guid invitationId)
         {
-            var result = await _mediator.Send(new ReinviteRequest() {InvitationId = invitationId});
+            var result = await _mediator.Send(new ReinviteRequest() { InvitationId = invitationId });
             if (result.Invited == false)
             {
                 return BadRequest();
             }
-            return RedirectToAction("Reinvited", "ConfirmCode", new {result.InvitationId});
+            return RedirectToAction("Reinvited", "ConfirmCode", new { result.InvitationId });
         }
 
         [HttpGet("/Invitations/Reinvite/{invitationId}")]
         public async Task<IActionResult> Reinvited(Guid invitationId)
         {
             var invitation = await _mediator.Send(new GetInvitationByIdRequest(invitationId), CancellationToken.None);
-            
-            return View("Reinvited", new ReinvitedViewModel(){Email = invitation.Email});
+
+            return View("Reinvited", new ReinvitedViewModel() { Email = invitation.Email });
         }
     }
 }

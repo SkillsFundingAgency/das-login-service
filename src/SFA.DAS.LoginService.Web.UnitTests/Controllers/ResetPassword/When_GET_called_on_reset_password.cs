a582f97c2b3f75b64c4d51c99b97d458f9021571
davidgouge <davegouge@googlemail.com>
2019-03-07 14:41:31 +0000

    More forgot password tests

--- src/SFA.DAS.LoginService.Web.UnitTests/Controllers/ResetPassword/When_GET_called_on_reset_password.cs
+++ src/SFA.DAS.LoginService.Web.UnitTests/Controllers/ResetPassword/When_GET_called_on_reset_password.cs
@@ -1,41 +1,11 @@
-using System;
 using System.Threading.Tasks;
 using FluentAssertions;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using NSubstitute;
 using NUnit.Framework;
-using SFA.DAS.LoginService.Application.ResetPassword;
 using SFA.DAS.LoginService.Web.Controllers.ResetPassword;
 
 namespace SFA.DAS.LoginService.Web.UnitTests.Controllers.ResetPassword
 {
-    [TestFixture]
-    public class ResetPasswordControllerTestBase
-    {
-        protected ResetPasswordController Controller;
-        protected Guid ClientId;
-        protected IMediator Mediator;
-
-        [SetUp]
-        public void Arrange()
-        {
-            Mediator = Substitute.For<IMediator>();
-            Controller = new ResetPasswordController(Mediator);
-            ClientId = Guid.NewGuid();
-        }
-    }
-    
-    public class When_POST_called_on_reset_password : ResetPasswordControllerTestBase
-    {
-        [Test]
-        public void Then_Request_is_passed_on_to_mediator()
-        {
-            var result = Controller.Post(ClientId, new ResetPasswordViewModel{Email = "forgot@password.com"});
-            Mediator.Received().Send(Arg.Is<ResetPasswordCodeRequest>(r => r.Email == "forgot@password.com" && r.ClientId == ClientId));
-        }
-    }
-    
     public class When_GET_called_on_reset_password : ResetPasswordControllerTestBase
     {
         private IActionResult _result;

fd0cca7228673859aaa2d2a5531b8e040b6cf678
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-05-17 09:00:18 +0100

    ON-1854 - Added Unique Index on [Password] & Default on [Id]

--- src/SFA.DAS.LoginService.Database/Tables/LoginService/LoginService.InvalidPasswords.sql
+++ src/SFA.DAS.LoginService.Database/Tables/LoginService/LoginService.InvalidPasswords.sql
@@ -1,5 +1,7 @@
 CREATE TABLE [LoginService].[InvalidPasswords](
-	[Id] [uniqueidentifier] NOT NULL,
-	[Password] [nvarchar](250) NOT NULL
+	[Id] [uniqueidentifier] NOT NULL DEFAULT NEWID(),
+	[Password] [nvarchar](250) NOT NULL 
 ) ON [PRIMARY] 
 GO
+
+CREATE UNIQUE CLUSTERED INDEX [IX_InvalidPasswords_Password] ON [LoginService].[InvalidPasswords] (Password)

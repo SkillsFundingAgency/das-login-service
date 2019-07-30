a3413bc4700756b54bd4b5a9868cfbd7291dd84a
Greg Barnes <gregdbarnes@gmail.com>
2019-07-30 12:27:03 +0100

    Update error message copy

--- src/SFA.DAS.LoginService.Application/ProcessLogin/ProcessLoginHandler.cs
+++ src/SFA.DAS.LoginService.Application/ProcessLogin/ProcessLoginHandler.cs
@@ -10,65 +10,65 @@ using SFA.DAS.LoginService.Data.Entities;
 
 namespace SFA.DAS.LoginService.Application.ProcessLogin
 {
-    public class ProcessLoginHandler : IRequestHandler<ProcessLoginRequest, ProcessLoginResponse>
+  public class ProcessLoginHandler : IRequestHandler<ProcessLoginRequest, ProcessLoginResponse>
+  {
+    private readonly IUserService _userService;
+    private readonly IEventService _eventService;
+    private readonly IIdentityServerInteractionService _interactionService;
+    private readonly LoginContext _loginContext;
+
+    public ProcessLoginHandler(IUserService userService, IEventService eventService,
+        IIdentityServerInteractionService interactionService, LoginContext loginContext)
     {
-        private readonly IUserService _userService;
-        private readonly IEventService _eventService;
-        private readonly IIdentityServerInteractionService _interactionService;
-        private readonly LoginContext _loginContext;
-
-        public ProcessLoginHandler(IUserService userService, IEventService eventService,
-            IIdentityServerInteractionService interactionService, LoginContext loginContext)
-        {
-            _userService = userService;
-            _eventService = eventService;
-            _interactionService = interactionService;
-            _loginContext = loginContext;
-        }
-
-        public async Task<ProcessLoginResponse> Handle(ProcessLoginRequest request, CancellationToken cancellationToken)
-        {
-            var context = await _interactionService.GetAuthorizationContextAsync(request.ReturnUrl);
-            if (context == null)
-            {
-                return new ProcessLoginResponse(){Message = "Invalid ReturnUrl"};
-            }
-            
-            var signInResult = await _userService.SignInUser(request.Username, request.Password, request.RememberLogin);
-
-            if (!signInResult.Succeeded)
-            {
-                _loginContext.UserLogs.Add(new UserLog()
-                {
-                    Id = GuidGenerator.NewGuid(), 
-                    Action = "Login", 
-                    Email = request.Username,  
-                    DateTime = SystemTime.UtcNow(),
-                    Result = "Login Invalid"
-                });
-                await _loginContext.SaveChangesAsync(cancellationToken);
-                
-                return signInResult.IsLockedOut 
-                    ? new ProcessLoginResponse(){Message = "User account is locked out"} 
-                    : new ProcessLoginResponse(){Message = "Make sure you've entered the correct email address and password. If you've forgotten your password you can reset it by clicking 'I have forgotten my password' below."};
-            }
-
-            var user = await _userService.FindByUsername(request.Username);
-
-            await _eventService.RaiseAsync(new UserLoginSuccessEvent(user.UserName, user.Id, user.UserName));
-            
-            var userLog = new UserLog()
-            {
-                Id = GuidGenerator.NewGuid(), 
-                Action = "Login", 
-                Email = request.Username,  
-                DateTime = SystemTime.UtcNow(),
-                Result = "Login OK"
-            };
-            _loginContext.UserLogs.Add(userLog);
-            await _loginContext.SaveChangesAsync(cancellationToken);
-            
-            return new ProcessLoginResponse() {CredentialsValid = true};
-        }
+      _userService = userService;
+      _eventService = eventService;
+      _interactionService = interactionService;
+      _loginContext = loginContext;
     }
+
+    public async Task<ProcessLoginResponse> Handle(ProcessLoginRequest request, CancellationToken cancellationToken)
+    {
+      var context = await _interactionService.GetAuthorizationContextAsync(request.ReturnUrl);
+      if (context == null)
+      {
+        return new ProcessLoginResponse() { Message = "Invalid ReturnUrl" };
+      }
+
+      var signInResult = await _userService.SignInUser(request.Username, request.Password, request.RememberLogin);
+
+      if (!signInResult.Succeeded)
+      {
+        _loginContext.UserLogs.Add(new UserLog()
+        {
+          Id = GuidGenerator.NewGuid(),
+          Action = "Login",
+          Email = request.Username,
+          DateTime = SystemTime.UtcNow(),
+          Result = "Login Invalid"
+        });
+        await _loginContext.SaveChangesAsync(cancellationToken);
+
+        return signInResult.IsLockedOut
+            ? new ProcessLoginResponse() { Message = "User account is locked out" }
+            : new ProcessLoginResponse() { Message = "Make sure you've entered the correct email address and password. If you've forgotten your password you can reset it by selecting 'I have forgotten my password' below." };
+      }
+
+      var user = await _userService.FindByUsername(request.Username);
+
+      await _eventService.RaiseAsync(new UserLoginSuccessEvent(user.UserName, user.Id, user.UserName));
+
+      var userLog = new UserLog()
+      {
+        Id = GuidGenerator.NewGuid(),
+        Action = "Login",
+        Email = request.Username,
+        DateTime = SystemTime.UtcNow(),
+        Result = "Login OK"
+      };
+      _loginContext.UserLogs.Add(userLog);
+      await _loginContext.SaveChangesAsync(cancellationToken);
+
+      return new ProcessLoginResponse() { CredentialsValid = true };
+    }
+  }
 }

5b71a8084fe285fb244a8d1eec4d7e29e149ff9c
gdbarnes <gregdbarnes@gmail.com>
2020-03-12 11:16:52 +0000

    Initial changes for cookie banner

--- src/SFA.DAS.LoginService.Web/wwwroot/javascripts/application.js
+++ src/SFA.DAS.LoginService.Web/wwwroot/javascripts/application.js
@@ -1,4 +1,20 @@
 ﻿window.GOVUKFrontend.initAll();
-window.DASFrontend.addCookieMessage();
 window.DASFrontend.passwordConditions.init();
 window.DASFrontend.showHidePassword.init();
+
+(function() {
+  "use strict";
+
+  var $cookieBanner = document.querySelector('[data-module="cookie-banner"]');
+  if ($cookieBanner != null) {
+    new CookieBanner($cookieBanner);
+  }
+
+  var $cookieSettings = document.querySelector(
+    '[data-module="cookie-settings"]'
+  );
+  if ($cookieSettings != null) {
+    var $cookieSettingsOptions = $cookieSettings.dataset.options;
+    new CookieSettings($cookieSettings, $cookieSettingsOptions);
+  }
+})(window);
